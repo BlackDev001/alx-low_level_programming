5ab0f14ab3b882701fd4586996f08d5c302ec989
BlackDev <flygirl4christ@outlook.com>
2023-02-24 12:49:02 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-is_upper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+ * _isupper: checks for uppercase characters
+ * Return: 1(for uppercase characters) or 0(for therwise)
+ */
+int _isupper(int c)
+{
+	char c;
+	if c = 'A';
+	{
+		_putchar("%c: %d\n", c, _isupper());
+		else
+		{
+			_putchar("%c: %d\n", c, _isupper());
+			return (0);
+		}
+		return (1);
+	}
+}
+
