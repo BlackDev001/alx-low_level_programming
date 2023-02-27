06cca9b40f9c4a3da8e392cbc84b0804c6d0f1b7
BlackDev <flygirl4christ@outlook.com>
2023-02-27 05:08:45 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts - Function that prints a string followed by \n to stdout
+ * @s: string to be printed
+ *
+ * Return: nothing
+ */
+void _puts(char *s)
+{
+	/* inputing string s for printing*/
+	_puts("%s");
+}
