68815dfe4eecd1106a5838e85aa15abcd030da59
BlackDev <flygirl4christ@outlook.com>
2023-03-08 04:00:36 -0800

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /*
- * _puts_recursion - prints a string followed by a new line
+ * _puts_recursion - prints string followed by a new line
  * @s: string
  */
 void _puts_recursion(char *s)
 {
 	char n;
 	if (s == 0)
+
 	{
 		return;
 	}
-	else
-	{
-		s[n + 1] = s[n + 1] + "\n";
-	}
-	
+	_puts_recursion(s + 1);
+	_putchar(*s);
 }
