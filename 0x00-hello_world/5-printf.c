afb273078bec86645638edb06ecb99677a0ea23a
BlackDev~ <flygirl4christ@outlook.com>
2023-06-14 23:22:04 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a line followed by a new line
+ * printf - should be used without puts
+ * Return: Alwayss 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
