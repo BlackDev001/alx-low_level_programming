a3f197d1fb694c07752a9ab69365a2376d8f7c3d
BlackDev~ <flygirl4christ@outlook.com>
2023-06-14 23:08:34 -0700

    Hello,puts

---
+++ 4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a function
+ * puts - should be used without printf
+ * Return: Always  0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
