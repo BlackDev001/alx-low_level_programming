4b1e117e390bc6ea6e6ed547568aa6004c246423
BlackDev~ <flygirl4christ@outlook.com>
2023-06-25 21:45:05 -0700

    programming

---
+++ 0x03-debugging/0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_rev - prints reversed string
+* @s: pointer
+* Return: void
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
